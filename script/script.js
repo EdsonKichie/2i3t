ea63570e0e239e14393f49dfe593fb258ae1a454
Edson Kichie <102360300+EdsonKichie@users.noreply.github.com>
2023-09-25 10:47:47 -0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,16 +1,14 @@
-function alo_mundo() {
-    alert("Projeto Iniciado! Alô!")
-}
-
 escrever = msg => alert(msg);
 let a = 0;
 let b = 0;
-let valor = 0;
+let valor = "";
 let executar = "";
-soma = (a, b) => a + b;
-sub = (a, b) => a - b;
-mult = (a, b) => a * b;
-div = (a, b) => a / b;
+temPonto = false;
+let desligada = true;
+soma = (a, b) => Number(a) + Number(b);
+sub = (a, b) => Number(a) - Number(b);
+mult = (a, b) => Number(a) * Number(b);
+div = (a, b) => Number(a) / Number(b);
 raiz = a => Math.sqrt(a);
 
 equacao2Grau = (a, b, c) => {
@@ -21,30 +19,64 @@ equacao2Grau = (a, b, c) => {
     return "x1 = " + div(soma(-b, raiz(delta)), mult(2, a)) +
         " x2 = " + div(sub(-b, raiz(delta)), mult(2, a));
 }
- function mostrar_resultado() {
+
+function mostrar_resultado() {
     document.getElementById("resultado").value = valor;
- }
-
-function calcular(){
-    if (executar != "") {
-        if (executar = "soma") mostrar_resultado( soma(a, b));
-        if (executar = "sub") mostrar_resultado(sub(a, b));
-        if (executar = "div") mostrar_resultado(div(a, b));
-        if (executar = "mult") mostrar_resultado(mullt(a, b));
-        executar = "";
-        a = "";
-        b = "";
-    }
-
 }
 
-function digitando(tecla){
-    if(executar == "") {
-        a += tecla;
-        alert (a);
-    } else {
-        b
+function calcular() {
+    if (executar != "") {
+        b = valor;
+        if (executar == "soma") valor = soma(a,b);
+        if (executar == "sub") valor = sub(a,b);
+        if (executar == "div") valor = div(a,b);
+        if (executar == "mult") valor = mult(a,b);
+        if (executar == "porc") valor = mult((div(a,100)),b);
+        if (executar == "raiz") valor = raiz(a);
+        mostrar_resultado();
+        executar = ""
+        a = "";
+        b = "";
+        valor = "";
     }
+}
+
+function desliga(){
+    if(desligada){
+        zerar();
+    } else{
+        zerar()
+        mostrar_resultado();
+    }
+    desligada = !desligada; 
+}
+function zerar(){
+    a = "";
+    b = "";
+    valor = "0";
+    executar = "";
+    mostrar_resultado();
+    valor = "";
+}
+
+function operacao(op){
+    executar = op;
+    a = valor;
+    valor = "";
+}
+
+
+function digitando(tecla) {
+    if (tecla == ".") {
+        if (!temPonto) {
+            valor = valor + tecla;
+            mostrar_resultado();
+            temPonto = true;
+        }
+        return
+    }
+    valor = valor + tecla;
+    mostrar_resultado();
 }
 
 
